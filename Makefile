ce452f6263a5a8af919a289fbffa5d58a6620990
Jørgen Andersen <jorg1piano@gmail.com>
2026-01-05 05:49:26 +0100

    Use test-data-3.sh

--- Makefile
+++ Makefile
@@ -1,2 +1,2 @@
 delete:
-	rm sqlite/test-db.db && ./sqlite/test-db.sh && ./sqlite/test-data-2.sh
+	rm sqlite/test-db.db && ./sqlite/test-db.sh && ./sqlite/test-data-3.sh
--- sqlite/test-data-3.sh
+++ sqlite/test-data-3.sh
@@ -12,11 +12,12 @@ if [ ! -f "$DB_NAME" ]; then
 fi
 
 echo "Inserting test data into test-db.db..."
-sqlite3 "$DB_NAME" < "$SCRIPT_DIR/exercises_complete.sql"
+ERROR_OUTPUT=$(sqlite3 "$DB_NAME" < "$SCRIPT_DIR/exercises_complete.sql" 2>&1)
 
 if [ $? -eq 0 ]; then
   echo "Test data inserted successfully"
 else
   echo "Failed to insert test data"
+  echo "Error: $ERROR_OUTPUT"
   exit 1
 fi
